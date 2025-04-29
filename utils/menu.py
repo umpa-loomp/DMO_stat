43106c9abcce98019e050a01fe6622ddde1a7f28
umpa-loomp <marchenko.dmytro@stud.onu.edu.ua>
2025-04-29 19:58:31 +0300

    S3

--- utils/menu.py
+++ utils/menu.py
@@ -44,6 +44,61 @@ def normality_submenu():
         else:
             print("\033[91mНекоректний вибір!\033[0m")
 
+def comparison_tests_submenu():
+    """Підменю вибору тестів для порівняння двох вибірок"""
+    while True:
+        print(
+            f"\n\033[92mТЕСТИ ДЛЯ ПОРІВНЯННЯ ДВОХ ВИБІРОК\033[0m\n"
+            f"\033[93m[1]\033[0m Тест Стюдента/Уелша (параметричний)\n"
+            f"\033[93m[2]\033[0m Тест Вілкоксона-Манна-Уітні (непараметричний)\n"
+            f"\033[93m[3]\033[0m Обидва тести послідовно\n"
+            f"\033[93m[0]\033[0m Повернутися до головного меню\n"
+        )
+
+        choice = input("\033[94mВиберіть тест (пусто - параметричний тест):\033[0m ")
+        
+        # Імпортуємо тут для уникнення циклічних імпортів
+        
+        if not choice or choice == "1":
+            print("\n--- Тест Стюдента/Уелша ---")
+            from tests.student_welch_test import student_welch_test
+            test_result = student_welch_test()
+            
+            # Пропонуємо спробувати непараметричний тест
+            if test_result:
+                print("\n\033[93mПорада:\033[0m Для порівняння результатів можна також виконати")
+                print("непараметричний тест Вілкоксона-Манна-Уітні, особливо якщо")
+                print("дані не відповідають нормальному розподілу.")
+                
+                run_nonparam = input("\033[94mБажаєте виконати непараметричний тест? (так/ні): \033[0m")
+                if run_nonparam.lower() == "так":
+                    print("\n--- Тест Вілкоксона-Манна-Уітні ---")
+                    from tests.wilcoxon_mann_whitney import wilcoxon_mann_whitney_test
+                    wilcoxon_mann_whitney_test()
+            return
+            
+        elif choice == "2":
+            print("\n--- Тест Вілкоксона-Манна-Уітні ---")
+            from tests.wilcoxon_mann_whitney import wilcoxon_mann_whitney_test
+            wilcoxon_mann_whitney_test()
+            return
+            
+        elif choice == "3":
+            print("\n--- Тест Стюдента/Уелша ---")
+            from tests.student_welch_test import student_welch_test
+            student_welch_test()
+            
+            print("\n--- Тест Вілкоксона-Манна-Уітні ---")
+            from tests.wilcoxon_mann_whitney import wilcoxon_mann_whitney_test
+            wilcoxon_mann_whitney_test()
+            return
+            
+        elif choice == "0":
+            return
+            
+        else:
+            print("\033[91mНекоректний вибір!\033[0m")
+
 def main_menu():
     while True:
         print(
@@ -51,7 +106,7 @@ def main_menu():
             f"\033[93m[1]\033[0m Перевірка даних на ПРОМАХИ\n"
             f"\033[93m[2]\033[0m Описова статистика\n"
             f"\033[93m[3]\033[0m Перевірка даних на НОРМАЛЬНИЙ розподіл: тести: [Шапіро, Андерсона]\n"
-            f"\033[93m[4]\033[0m Тест Стюдента для незалежних даних двох вибірок\n"
+            f"\033[93m[4]\033[0m Тести для порівняння двох вибірок: [Стюдента/Уелша, Вілкоксона-Манна-Уітні]\n"
             f"\033[93m[5]\033[0m ПАРНИЙ тест Стюдента для залежних даних\n"
             f"\033[93m[6]\033[0m Тест Тюкі\n"
             f"\033[93m[7]\033[0m Тест Данна\n"
@@ -76,8 +131,8 @@ def main_menu():
             normality_submenu()
 
         elif input_d == "4":
-            from tests.student_welch_test import student_welch_test
-            student_welch_test()
+            # Підменю вибору тесту порівняння вибірок
+            comparison_tests_submenu()
             
         elif input_d == "5":
             print("\nФункціональність у розробці\n")
